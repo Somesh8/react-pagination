2dd352bc28da249d7d82c1128b9d9ec533ab1eba
Somesh Arewar <Somesh.Arewar@blueconchtech.com>
2022-11-24 16:16:51 +0530

    intial code commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,282 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./component/header";
+import Content from "./component/content"
+import Footer from "./component/footer";
+import React, { useEffect, useState } from "react";
+import Table, { SelectColumnFilter, StatusPill, LocateCell } from "./component/table";
+import { Switch } from "@headlessui/react";
+import MyPagination from "./component/pagination";
+import { Route, Routes } from "react-router-dom";
+
+const getData = () => [
+  {
+    name: "Jane Cooper",
+    email: "jane.cooper@example.com",
+    title: "Regional Paradigm Technician",
+    department: "Optimization",
+    status: "Active",
+    role: "Admin",
+    locate: "Locate1",
+    posCoords: [260, 900],
+    imgUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Cody Fisher",
+    email: "cody.fisher@example.com",
+    title: "Product Directives Officer",
+    department: "Intranet",
+    status: "Active",
+    role: "Owner",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Esther Howard",
+    email: "esther.howard@example.com",
+    title: "Forward Response Developer",
+    department: "Directives",
+    status: "Active",
+    role: "Member",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Jenny Wilson",
+    email: "jenny.wilson@example.com",
+    title: "Central Security Manager",
+    department: "Program",
+    status: "Active",
+    role: "Member",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Kristin Watson",
+    email: "kristin.watson@example.com",
+    title: "Lean Implementation Liaison",
+    department: "Mobility",
+    status: "Active",
+    role: "Admin",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Cameron Williamson",
+    email: "cameron.williamson@example.com",
+    title: "Internal Applications Engineer",
+    department: "Security",
+    status: "Active",
+    role: "Member",
+    locate: "Locate",
+    posCoords: [260, 900],
+    imgUrl:
+      "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Cody Fisher",
+    email: "cody.fisher@example.com",
+    title: "Product Directives Officer",
+    department: "Intranet",
+    status: "Active",
+    role: "Owner",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Esther Howard",
+    email: "esther.howard@example.com",
+    title: "Forward Response Developer",
+    department: "Directives",
+    status: "Active",
+    role: "Member",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Jenny Wilson",
+    email: "jenny.wilson@example.com",
+    title: "Central Security Manager",
+    department: "Program",
+    status: "Active",
+    role: "Member",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Kristin Watson",
+    email: "kristin.watson@example.com",
+    title: "Lean Implementation Liaison",
+    department: "Mobility",
+    status: "Active",
+    role: "Admin",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Cameron Williamson",
+    email: "cameron.williamson@example.com",
+    title: "Internal Applications Engineer",
+    department: "Security",
+    status: "Active",
+    role: "Member",
+    locate: "Locate",
+    posCoords: [260, 900],
+    imgUrl:
+      "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Cody Fisher",
+    email: "cody.fisher@example.com",
+    title: "Product Directives Officer",
+    department: "Intranet",
+    status: "Active",
+    role: "Owner",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Esther Howard",
+    email: "esther.howard@example.com",
+    title: "Forward Response Developer",
+    department: "Directives",
+    status: "Active",
+    role: "Member",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Jenny Wilson",
+    email: "jenny.wilson@example.com",
+    title: "Central Security Manager",
+    department: "Program",
+    status: "Active",
+    role: "Member",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Kristin Watson",
+    email: "kristin.watson@example.com",
+    title: "Lean Implementation Liaison",
+    department: "Mobility",
+    status: "Active",
+    role: "Admin",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  },
+  {
+    name: "Cameron Williamson",
+    email: "cameron.williamson@example.com",
+    title: "Internal Applications Engineer",
+    department: "Security",
+    status: "Active",
+    role: "Member",
+    locate: "Locate",
+    posCoords: [260, 900],
+    imgUrl:
+      "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+  }
+];
+
 
 function App() {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "name"
+      },
+      {
+        Header: "Title",
+        accessor: "title"
+      },
+      {
+        Header: "Locate",
+        accessor: "locate",
+        Cell: LocateCell,
+        posAccessor: "posCoords"
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+        Cell: StatusPill
+      },
+      {
+        Header: "Role",
+        accessor: "role",
+        Filter: SelectColumnFilter,
+        filter: "includes"
+      }
+    ],
+    []
+  );
+  const companyColumns = React.useMemo(
+    () => [
+      {
+        Header: "Id",
+        accessor: "company_id"
+      },
+      {
+        Header: "Name",
+        accessor: "company_name"
+      },
+      {
+        Header: "Description",
+        accessor: "company_desc"
+      }
+    ],
+    []
+  );
+  let companyData = []
+  const [compData, setCompData] = useState([]);
+
+  const getCompanyData = () => {
+    fetch(`http://localhost:8081/company/`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCompData(data);
+        console.log("AAAA", data);
+      });
+  }
+
+  useEffect(() => {
+    // getCompanyData();
+    console.log("loading")
+  }, [])
+
+  const data = React.useMemo(() => getData(), []);
+  // const companyData = React.useMemo(() => getCompanyData(), []);
+  // console.log("CPMP ---> ", companyData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      {/* <Content /> */}
+      <Routes>
+        {/* <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} /> */}
+        <Route path="/pagination" element={<MyPagination/>} />
+      </Routes>
+      {/* <Table columns={columns} data={data} /> */}
+      {/* <Table columns={companyColumns} data={compData} /> */}
+      <Footer />
+      {/* <div className="relative bg-white">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div> */}
+    </>
   );
 }
 
---
+++ src/component/pagination.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import Table, { SelectColumnFilter, StatusPill, LocateCell } from "./table";
+
+function MyPagination() {
+  const companyColumns = React.useMemo(
+    () => [
+      {
+        Header: "Id",
+        accessor: "company_id"
+      },
+      {
+        Header: "Name",
+        accessor: "company_name"
+      },
+      {
+        Header: "Description",
+        accessor: "company_desc"
+      }
+    ],
+    []
+  );
+  const [compData, setCompData] = useState([]);
+//   const [totalData, setTotalData] = useState(0);
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [pageSize, setPageSize] = useState(5);
+
+  let initPageData = {compData: [], totalData:0, currentPage:1, pageSize:3};
+
+  const [currentPageData, setCurrentPageData] = useState(initPageData);
+
+  const updatePageSize = (size) => {
+    console.log("upadating Size");
+    setCurrentPageData({...currentPageData, pageSize:size})
+    getCompanyData(currentPageData.currentPage, size);
+    console.log("DATA --> ",currentPageData)
+    // { ...openWord, pol: true }
+  }
+
+  const updatePageNumber = (direction) => {
+    console.log("upadating Number ", direction)
+    var first = 1;
+    var last = Math.ceil(currentPageData.totalData / currentPageData.pageSize);
+    if(direction === "next") {
+      setCurrentPageData({...currentPageData, currentPage: ++currentPageData.currentPage});
+    }
+    else if(direction === "previous") {
+      setCurrentPageData({...currentPageData, currentPage: --currentPageData.currentPage});
+    }
+    else if(direction === "first") {
+      // setCurrentPageData({...currentPageData, currentPage: first});
+      currentPageData.currentPage = first;
+      setCurrentPageData(currentPageData);
+    }
+    else if(direction === "last") {
+      currentPageData.currentPage = last;
+      // setCurrentPageData({...currentPageData, currentPage: last});
+      setCurrentPageData(currentPageData);
+
+    }
+    // setCurrentPageData({...currentPageData, pageSize:size})
+    console.log("DATA --> ",currentPageData)
+    getCompanyData(currentPageData.currentPage, currentPageData.pageSize);
+  }
+
+  const getCompanyData = (currentPage, pageSize) => {
+    fetch(`http://localhost:8081/company/pagination?page=${currentPage}&size=${pageSize}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("ABCD", data);
+        // setCompData(data);
+        setCurrentPageData({compData: data.data, totalData: data.totalItems, currentPage: data.currentPage, pageSize})
+      });
+  }
+
+  useEffect(() => {
+    getCompanyData(1, 3);
+    console.log("loading from pagination ->  ",currentPageData);
+  }, [])
+
+  useEffect(() => {
+    console.log("loading from pagination 2  ->  ",currentPageData);
+  }, [currentPageData])
+
+  return (
+    <>
+      <Table updatePageNumber={updatePageNumber} updatePageSize={updatePageSize} columns={companyColumns} data={currentPageData.compData} currentPageData={currentPageData} />
+    </>
+  );
+}
+
+export default MyPagination;
